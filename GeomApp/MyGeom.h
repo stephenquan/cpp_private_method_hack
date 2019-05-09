26638a9b86de62424855c516e8e3030bdb40439c
Stephen Quan <squan@esri.com>
2019-05-10 08:58:48 +1000

    Renamed as Geom_friendly.h to reinforce 'friend' solution

--- GeomApp/MyGeom.h
+++ GeomApp/MyGeom.h
@@ -1,8 +1,17 @@
 #ifndef __MyGeom_H__
 #define __MyGeom_H__
 
-//#include "../GeomLib/geom.h"
-#include "Geom_patched.h"
+// #include "../GeomLib/geom.h"
+//
+// The above #include yields the following errors:
+//
+// C:\GitHub\cpp_private_method_hack\GeomApp\MyGeom.cpp(11): error C2248: 'Geom::_area': cannot access private member declared in class 'Geom'
+// c:\github\cpp_private_method_hack\geomapp\../GeomLib/geom.h(30): note: see declaration of 'Geom::_area'
+// c:\github\cpp_private_method_hack\geomapp\../GeomLib/geom.h(6): note: see declaration of 'Geom'
+
+// This is a workaround with the friend forced.
+
+#include "Geom_friendly.h"
 
 class MyGeom : public Geom
 {
